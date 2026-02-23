66b7d642bdfb99645c865a8379cc06e93335162c
Tiago Vicente <tiagovicente2@gmail.com>
2026-02-23 01:13:34 -0300

    fix(deploy): add base path rewriting for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,7 @@
 import { defineConfig } from 'astro/config';
 import svelte from '@astrojs/svelte';
 
-const isProduction = process.env.NODE_ENV === 'production';
-
 export default defineConfig({
   integrations: [svelte()],
-  site: 'https://tiagovicente2.github.io',
-  base: isProduction ? '/arpeggios' : '/',
   output: 'static',
 });
