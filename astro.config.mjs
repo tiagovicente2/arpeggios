0b8dfb8870ca041b3fd5ccfb6a5071474b3b6b21
Tiago Vicente <tiagovicente2@gmail.com>
2026-02-23 01:06:50 -0300

    fix(config): support both localhost and GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 import { defineConfig } from 'astro/config';
 import svelte from '@astrojs/svelte';
 
+const isProduction = process.env.NODE_ENV === 'production';
+
 export default defineConfig({
   integrations: [svelte()],
-  site: 'https://tiagovicente2.github.io/arpeggios',
+  site: 'https://tiagovicente2.github.io',
+  base: isProduction ? '/arpeggios' : '/',
   output: 'static',
 });
